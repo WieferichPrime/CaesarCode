d864de710f9261c2e767cd982edf9598b8841353
SupaProgramer <dania.swirdov@gmail.com>
2021-09-16 22:49:16 +0300

    'com'

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from war import war
 
 
 def main():
-    message = input('Введите слово для шифрования\n').split(' ')[0].lower()
-    key = int(input('Введите ключ (целое число)\n').split(' ')[0])
-    keyword = input('Введите ключевое слово\n').split(' ')[0].lower()
-    print(encode(message, keyword, key))
+    # message = input('Введите слово для шифрования\n').split(' ')[0].lower()
+    # key = int(input('Введите ключ (целое число)\n').split(' ')[0])
+    # keyword = input('Введите ключевое слово\n').split(' ')[0].lower()
+    # print(encode(message, keyword, key))
     print('Идёт зашифровка и расшифровка главы Войны и Мир...')
     war()
     print('Глава зашифрована и расшифрована, расшифрованный текст хранится в файле war_decoded.txt')
--- war.py
+++ war.py
@@ -1,6 +1,7 @@
 import re
 from encode import encode
 from collections import Counter
+from frequency import frequency as frec
 
 
 def war():
@@ -21,6 +22,23 @@ def war():
         else:
             pos += 1
 
+    charsCount = Counter()
+    frequency = dict(sorted(list(frec().items()), key=lambda item: item[1], reverse=True))
+    frequencyKeys = list(frequency.keys())
+    for char in text:
+        if char in frequencyKeys:
+            charsCount[char] += 1
+
+    decoder = {}
+
+    for i in range(len(frequency)):
+        decoder[charsCount[i]] = frequencyKeys[i]
+
+    charsCount = list(dict(charsCount.most_common()).keys())
+    for char in text:
+        if char in frequencyKeys:
+            decoder[char] = frequencyKeys[charsCount.index(char)]
+
     bigramms = Counter()
     open('war_encoded.txt', 'w').write(text)
 
@@ -28,12 +46,16 @@ def war():
         for i in range(len(word) - 1):
             bigramms[word[i] + word[i + 1]] += 1
 
-    bigramms = list(map(lambda item: item[0], bigramms.most_common()))
-    decoder = {}
+    bigramms = list(map(lambda item: item[0], bigramms.most_common(20)))
+
+
+    bigrammsDecoder = {}
     for i in range(len(bigramms)):
-        if bigramms[i][0] not in decoder.keys() and bigramms[i][1] not in decoder.keys():
-            decoder[bigramms[i][0]] = bigrammsGlobalOrder[i][0]
-            decoder[bigramms[i][1]] = bigrammsGlobalOrder[i][1]
+        if bigramms[i][0] not in bigrammsDecoder.keys() and bigramms[i][1] not in bigrammsDecoder.keys():
+            bigrammsDecoder[bigramms[i][0]] = bigrammsGlobalOrder[i][0]
+            bigrammsDecoder[bigramms[i][1]] = bigrammsGlobalOrder[i][1]
+
+    decoder = decoder | bigrammsDecoder
 
     pos = 0
     decoderKeys = decoder.keys()
@@ -46,3 +68,4 @@ def war():
             pos += 1
 
     open('war_decoded.txt', 'w').write(text)
+
