4ccf3a61aa5fc41c07529b0f870bd7ca3f9dbcb1
SupaProgramer <dania.swirdov@gmail.com>
2021-09-16 21:47:05 +0300

    'commit2'

---
+++ encode.py
@@ -0,0 +1,22 @@
+from frequency import frequency
+
+
+def encode(word = 'сообщение', keyword = 'ключ', key = 3):
+    if not (len(keyword) <= 32 - key and len(set(keyword)) == len(keyword)):
+        return 'Некорректные входные значения'
+    encoded = ''
+    i = 0
+    subst = ''
+    ru = ''.join(frequency().keys())
+    while len(subst) < key:
+        if (ru[len(ru) - i - 1]) not in keyword:
+            subst += ru[len(ru) - i - 1]
+        i += 1
+
+    encoded += subst[::-1]
+    for char in keyword:
+        encoded += char
+    for char in ru:
+        if char not in encoded:
+            encoded += char
+    return ''.join(list(map(lambda char: encoded[ru.find(char)], word)))
--- war.py
+++ war.py
@@ -1,46 +1,48 @@
 import re
-from main import encode
+from encode import encode
 from collections import Counter
 
-f = open('warFirstPart.txt')
-text = f.read().lower()
-bigrammsGlobalOrder = Counter()
-for word in re.findall(r'[а-яё]+', text):
-    for i in range(len(word) - 1):
-        bigrammsGlobalOrder[word[i] + word[i + 1]] += 1
-bigrammsGlobalOrder = list(map(lambda item: item[0], bigrammsGlobalOrder.most_common()))
 
-pos = 0
-while pos < len(text):
-    match = re.compile(r'[а-яё]+').match(text , pos)
-    if match:
-        text = text[:match.start()] + encode(word=match[0]) + text[match.end():]
-        pos = match.end()
-    else:
-        pos += 1
+def war():
+    f = open('warFirstPart.txt')
+    text = f.read().lower()
+    bigrammsGlobalOrder = Counter()
+    for word in re.findall(r'[а-яё]+', text):
+        for i in range(len(word) - 1):
+            bigrammsGlobalOrder[word[i] + word[i + 1]] += 1
+    bigrammsGlobalOrder = list(map(lambda item: item[0], bigrammsGlobalOrder.most_common()))
 
-bigramms = Counter()
-open('war_encoded.txt', 'w').write(text)
+    pos = 0
+    while pos < len(text):
+        match = re.compile(r'[а-яё]+').match(text , pos)
+        if match:
+            text = text[:match.start()] + encode(word=match[0]) + text[match.end():]
+            pos = match.end()
+        else:
+            pos += 1
 
-for word in re.findall(r'[а-яё]+', text):
-    for i in range(len(word) - 1):
-        bigramms[word[i] + word[i + 1]] += 1
+    bigramms = Counter()
+    open('war_encoded.txt', 'w').write(text)
 
-bigramms = list(map(lambda item: item[0], bigramms.most_common()))
-decoder = {}
-for i in range(len(bigramms)):
-    if bigramms[i][0] not in decoder.keys() and bigramms[i][1] not in decoder.keys():
-        decoder[bigramms[i][0]] = bigrammsGlobalOrder[i][0]
-        decoder[bigramms[i][1]] = bigrammsGlobalOrder[i][1]
+    for word in re.findall(r'[а-яё]+', text):
+        for i in range(len(word) - 1):
+            bigramms[word[i] + word[i + 1]] += 1
 
-pos = 0
-decoderKeys = decoder.keys()
-while pos < len(text):
-    match = re.compile(r'[а-яё]').match(text, pos)
-    if match and match[0] in decoderKeys:
-        text = text[:match.start()] + decoder[match[0]] + text[match.end():]
-        pos = match.end()
-    else:
-        pos += 1
+    bigramms = list(map(lambda item: item[0], bigramms.most_common()))
+    decoder = {}
+    for i in range(len(bigramms)):
+        if bigramms[i][0] not in decoder.keys() and bigramms[i][1] not in decoder.keys():
+            decoder[bigramms[i][0]] = bigrammsGlobalOrder[i][0]
+            decoder[bigramms[i][1]] = bigrammsGlobalOrder[i][1]
 
-open('war_decoded.txt', 'w').write(text)
+    pos = 0
+    decoderKeys = decoder.keys()
+    while pos < len(text):
+        match = re.compile(r'[а-яё]').match(text, pos)
+        if match and match[0] in decoderKeys:
+            text = text[:match.start()] + decoder[match[0]] + text[match.end():]
+            pos = match.end()
+        else:
+            pos += 1
+
+    open('war_decoded.txt', 'w').write(text)
